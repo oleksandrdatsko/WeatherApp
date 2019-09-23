907f2e52b110ad582936444d4ab8a6939e2ad7a5
oleksandrdatsko <54150627+oleksandrdatsko@users.noreply.github.com>
2019-09-23 06:48:13 +0200

    updated GetWeatherDataModel - added GetURL() method

--- WeatherApp/Models/GetWeatherDataModel.cs
+++ WeatherApp/Models/GetWeatherDataModel.cs
@@ -13,19 +13,23 @@ namespace WeatherApp.Models
 
         public async Task<WeatherModel> LoadWeather()
         {
-            string url = "";
-            if( DefaultValuesModel.APIKey != "")
-            {
-                url = $"http://api.openweathermap.org/data/2.5/weather?id={ DefaultValuesModel.CityID }&appid={ DefaultValuesModel.APIKey }&units={ DefaultValuesModel.Units }";
-            }
-            else
-            {
-                url = "https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=b6907d289e10d714a6e88b30761fae22";
-            }
+            string url = GetURL();
 
             WeatherModel response = await APIProcessor<WeatherModel>.APICall(url);
 
             return response;
+        }
+
+        private string GetURL()
+        {
+            if (DefaultValuesModel.APIKey != "")
+            {
+                return $"http://api.openweathermap.org/data/2.5/weather?id={ DefaultValuesModel.CityID }&appid={ DefaultValuesModel.APIKey }&units={ DefaultValuesModel.Units }";
+            }
+            else
+            {
+                return "https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=b6907d289e10d714a6e88b30761fae22";
+            }
         }
 
     }
