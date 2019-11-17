8caeebe537f0af0d4191579273cc28c0ec965b78
oleksandrdatsko <54150627+oleksandrdatsko@users.noreply.github.com>
2019-11-17 21:26:58 +0100

    added Test Project for OpenWeatherAPI project

---
+++ OpenWeatherAPI.UnitTests/APIHelperTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace OpenWeatherAPI.UnitTests
+{
+    [TestClass]
+    public class APIHelperTests
+    {
+        [TestMethod]
+        public void APIClient_Scenario_Initialize_ReturnsTrue()
+        {
+            // Arrange
+            APIHelper.InitializeClient();
+            // Act
+
+            // Asert
+            Assert.IsNotNull(APIHelper.APIClient);
+            
+        }
+
+        [TestMethod]
+
+        public async Task APIClient_Scenario_GetResponse_ReturnSuccess()
+        {
+            APIHelper.InitializeClient();
+
+            string url = "https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=b6907d289e10d714a6e88b30761fae22";
+            HttpResponseMessage response = await APIHelper.APIClient.GetAsync(url);
+            HttpStatusCode expectedResponseCode = HttpStatusCode.OK;
+            Task.WaitAll();
+
+            Assert.AreEqual(response.StatusCode, expectedResponseCode);
+
+        }
+    }
+}
---
+++ OpenWeatherAPI.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OpenWeatherAPI.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OpenWeatherAPI.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("78b15028-0a70-48a3-98af-fa2b12395a54")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
