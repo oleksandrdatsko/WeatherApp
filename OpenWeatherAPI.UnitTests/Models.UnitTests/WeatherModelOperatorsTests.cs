d1831457f19a38dd92631c84f413bc60f1f661a8
oleksandrdatsko <54150627+oleksandrdatsko@users.noreply.github.com>
2019-12-01 19:54:57 +0100

    Added file with test for WeatherModel operator overloads. The file is formated with regions for each property.

---
+++ OpenWeatherAPI.UnitTests/Models.UnitTests/WeatherModelOperatorsTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using OpenWeatherAPI.Models;
+
+namespace OpenWeatherAPI.UnitTests.Models.UnitTests
+{
+    [TestClass]
+    public class WeatherModelOperatorsTests
+    {
+
+        private string jsonWeatherResponse = "";
+
+        private void ReadWeatherResponseJson()
+        {
+            using (StreamReader streamReader = new StreamReader(@"D:\Visual Studio Projects\MVVM_WPF\OpenWeatherAPI.UnitTests\Resources\SampleWeatherResponse.json"))
+            {
+                jsonWeatherResponse = streamReader.ReadToEnd();
+            }
+        }
+
+        #region sys
+
+        [TestMethod]
+        public void WeatherModel_Scenario_CheckSys_CountryOperatorEquals_ReturnTrue()
+        {
+            //Arrange
+            ReadWeatherResponseJson();
+
+            //Act
+            WeatherModel weather = JsonConvert.DeserializeObject<WeatherModel>(jsonWeatherResponse);
+            string actualCountry = weather.sys.Country;
+            string expectedCountry = "AU";
+            bool comparisonResult = (actualCountry == expectedCountry);
+
+            //Assert
+            Assert.IsTrue(comparisonResult);
+
+        }
+
+        [TestMethod]
+        public void WeatherModel_Scenario_CheckSys_CountryOperatorEquals_ReturnFalse()
+        {
+            //Arrange
+            ReadWeatherResponseJson();
+
+            //Act
+            WeatherModel weather = JsonConvert.DeserializeObject<WeatherModel>(jsonWeatherResponse);
+            string actualCountry = weather.sys.Country;
+            string expectedCountry = "PL";
+            bool comparisonResult = (actualCountry == expectedCountry);
+
+            //Assert
+            Assert.IsFalse(comparisonResult);
+
+        }
+
+        [TestMethod]
+        public void WeatherModel_Scenario_CheckSys_CountryOperatorNotEquals_ReturnTrue()
+        {
+            //Arrange
+            ReadWeatherResponseJson();
+
+            //Act
+            WeatherModel weather = JsonConvert.DeserializeObject<WeatherModel>(jsonWeatherResponse);
+            string actualCountry = weather.sys.Country;
+            string expectedCountry = "PL";
+            bool comparisonResult = (actualCountry != expectedCountry);
+
+            //Assert
+            Assert.IsTrue(comparisonResult);
+
+        }
+
+        [TestMethod]
+        public void WeatherModel_Scenario_CheckSys_CountryOperatorNotEquals_ReturnFalse()
+        {
+            //Arrange
+            ReadWeatherResponseJson();
+
+            //Act
+            WeatherModel weather = JsonConvert.DeserializeObject<WeatherModel>(jsonWeatherResponse);
+            string actualCountry = weather.sys.Country;
+            string expectedCountry = "AU";
+            bool comparisonResult = (actualCountry != expectedCountry);
+
+            //Assert
+            Assert.IsFalse(comparisonResult);
+
+        }
+
+        #endregion
+
+    }
+}
